7a1e72db7c5e9351e25ddddbe956525e502bd92f
nefelims <nefemissiou@dmst.aueb.gr>
2024-11-17 23:01:30 +0200

    Update BlueLagoonAM.java

--- BlueLagoonAM.java
+++ BlueLagoonAM.java
@@ -5,5 +5,6 @@ public class BlueLagoonAM {
         System.out.println("8230036");
         System.out.println("8230116");
         System.out.println("8230167");
+        System.out.println("8230096");
     }
 }
