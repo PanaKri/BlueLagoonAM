f45d2bf38b59fe5d5c540c099ebbad150c4e9994
eleni14 <elenikalifo@gmail.com>
2024-11-18 16:46:18 +0200

    Update BlueLagoonAM.java

--- BlueLagoonAM.java
+++ BlueLagoonAM.java
@@ -7,6 +7,6 @@ public class BlueLagoonAM {
         System.out.println("8230167");
         System.out.println("8230096");
         System.out.println("8230221");
-        system.out.println("8230229");
+        System.out.println("8230229");
     }
 }
